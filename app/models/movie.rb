672da84d5068dfe481e56c20d80d6ed4ab9cca3e
Claudia Eachus <ceachus@users.noreply.github.com>
2017-05-18 14:46:25 -0500

    save

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,12 @@ class Movie < ApplicationRecord
   #  - duration: must be integer between 0 and 2764800
   validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to=>2764800}
   #  - description: no rules
-  
+
   #  - image_url: no rules
+
+  belongs_to(:director, :class_name =>"Director", :foreign_key => "director_id")
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+
+  has_many :characters
+  has_many :actors, :through => :characters
 end
